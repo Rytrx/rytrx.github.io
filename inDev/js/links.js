977db811b65affd7e655599879e9f0254326dcc7
Darryl Stacey <Darryl.Stacey@liberty-it.co.uk>
2017-02-03 15:46:21 +0000

    Spotlight search results

---
+++ inDev/js/links.js
@@ -0,0 +1,19 @@
+var links = [
+    ["Facebook", "http://facebook.com/", "", ["fb"]],
+    ["/r/dota2", "https://www.reddit.com/r/dota2/", ""],
+    ["/r/worldnews", "https://www.reddit.com/r/worldnews/", "", ["wn"]],
+    ["GitHub", "https://github.com/", ""],
+    ["Stack Overflow", "http://stackoverflow.com/", "", ["so"]]
+];
+
+//Urls for search functionality
+//'{Q}' will be replaced by search query
+var searchSources = [
+    ["Google", "https://www.google.com/#q={Q}"],
+    ["Google Images", "https://www.google.com/search?tbm=isch&q={Q}"],
+    ["IMDB", "http://www.imdb.com/find?q={Q}"],
+    ["Stack Overflow", "http://stackoverflow.com/search?q={Q}"],
+    ["Urban Dictionary", "http://www.urbandictionary.com/define.php?term={Q}}"],
+    ["Wikipedia", "http://en.wikipedia.org/w/index.php?search={Q}",],
+    ["YouTube", "https://www.youtube.com/results?search_query={Q}"]
+];
--- inDev/js/script.js
+++ inDev/js/script.js
@@ -5,6 +5,8 @@ var $ = function (id) {
 
 //core element vars
 var dateDiv = $('dateContainer');
+var searchList = $('searchList');
+var spotlight = $('spotlight');
 
 function init() {
     buildDate();
@@ -14,9 +16,16 @@ function init() {
     $('dateContainer').style.opacity = 1;
 }
 
+/* ================================ */
+/* ======== Date Widget ========== */
+/* ============================== */
+
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var dayNames = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
 
+/** 
+ * Build date display 
+ */
 function buildDate() {
     var today = new Date();
     dateDiv.innerHTML = "<font class=\"font-3em\">" +
@@ -29,14 +38,108 @@ function buildDate() {
         today.getFullYear() +
         "</font>";
 }
+/* -------------------------------- */
+
+/* ================================ */
+/* ====== Spotlight Search ======= */
+/* ============================== */
+
+/**
+ * Handles keyboard input in spotlight search box
+ */
+function handleQuery(event, query) {
+    var key = event.keyCode || event.which;
+
+    if (key === 27) { //escape
+        spotlight.value = "";
+        buildSpotlightResults("");
+
+    } else if (key === 13) { //enter
+        navigate();
+
+    } else if (key === 38 || key === 40) {
+        highlightOption(key);
+
+    } else {
+        buildSpotlightResults(query.trim());
+    }
+
+}
+
+/**
+ * Build spotlight results
+ */
+function buildSpotlightResults(query) {
+    if (query !== "") {
+
+        searchList.innerHTML = buildLinkOptions(query) + buildSearchOptions(query);
+
+    } else {
+        searchList.innerHTML = "";
+    }
+}
+
+/**
+ * Build link options list
+ */
+function buildLinkOptions(query) {
+    var linkOptions = "";
+
+    if (query.length >= 2) {
+        for (var i = 0; i < links.length; i++) {
+            if (links[i][0].toLowerCase().includes(query.toLowerCase())) {
+                linkOptions += "<li><span>" + links[i][0] + "</span></li>";
+            }
+        }
+    }
+
+    return linkOptions;
+}
 
 
+/**
+ * Build search options list
+ */
+function buildSearchOptions(query) {
+    var maxDisplay = 40;
 
+    var viewText = "";
+    if (query.length > maxDisplay) {
+        viewText = query.substring(0, maxDisplay) + "...";
+    } else {
+        viewText = query;
+    }
 
+    var searchOptions = "";
+    for (var i = 0; i < searchSources.length; i++) {
+        searchOptions += "<li><span>Search " + searchSources[i][0] + " for " + viewText + "</span></li>";
+    }
 
+    return searchOptions;
+}
 
+/**
+ * Navigate to link or search option
+ */
+function navigate() {
+    window.location = searchSources[0][1].replace("{Q}", encodeURIComponent(spotlight.value));
+}
 
+/**
+ * Highlight link / search option
+ */
+function highlightOption(key) {
+    var index = -1;
 
+    if (key === 38) {
+        index--;
+    } else {
+        index++;
+    }
 
+    alert(index);
+}
+
+/* -------------------------------- */
 
 
